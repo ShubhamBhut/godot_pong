ecfed69104952639c506592134bcbaebda7bc980
ShubhamBhut <shubhamprecious1@gmail.com>
2024-07-23 09:53:22 +0530

    Countdown timer added

--- Ball/Ball.gd
+++ Ball/Ball.gd
@@ -12,3 +12,12 @@ func _physics_process(delta: float):
 	var collision_object = move_and_collide(ball_velocity * ball_speed * delta)
 	if collision_object:
 		ball_velocity = ball_velocity.bounce(collision_object.get_normal())
+
+func stop_ball():
+	ball_speed = 0
+
+func restart_ball():
+	ball_speed = 800
+	ball_velocity.x = [-1, 1][randi() % 2]
+	ball_velocity.y = [-0.7, 0.7][randi() % 2]
+
--- Level/Level.gd
+++ Level/Level.gd
@@ -3,15 +3,25 @@ extends Node
 var PlayerScore = 0
 var OpponentScore = 0
 
+func score_achieved():
+	$Ball.position = Vector2(640, 360)
+	get_tree().call_group('BallGroup', 'stop_ball')
+	$CountdownTimer.start()
+	$CountdownLabel.visible = true
 
 func _on_left_body_entered(body: Node2D) -> void:
-	$Ball.position = Vector2(640, 360)
+	score_achieved()
 	OpponentScore += 1
 
 func _on_right_body_entered(body: Node2D) -> void:
-	$Ball.position = Vector2(640, 360)
+	score_achieved()
 	PlayerScore += 1
 
 func _process(delta: float) -> void:
 	$PlayerScore.text = str(PlayerScore)
 	$OpponentScore.text = str(OpponentScore)
+	$CountdownLabel.text = str(int($CountdownTimer.time_left)+1)
+
+func _on_countdown_timer_timeout() -> void:
+	get_tree().call_group('BallGroup', 'restart_ball')
+	$CountdownLabel.visible = false
